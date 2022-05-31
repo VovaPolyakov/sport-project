afb66e8bf0fc1d32521284fed69347cfae0e3102
Vladimir <1337vovalenovo@gmail.com>
2022-05-31 03:01:49 +0300

    added adaptive for tablet and phone

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ next.addEventListener('click',function(event){
     }
 })
 function showSlides(n) {
-
-    let slides = document.querySelectorAll('.mySlides');
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.lengt
-    }
-    for (let i = 0; i < slides.length; i++) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[slideIndex-1].style.display = "block";
 }
+
+
+
